54a82e24345e141f777198ec727e103b2cb9904b
Jakob Fries <jakob.fries@gmail.com>
2014-03-24 18:49:33 +0100

    switched to clang in build_linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,1 +1,1 @@
-g++ -g main.cpp file.cpp mullib.cpp net.cpp serialize.cpp -lGL -lGLU -lSDL2 -lpthread -D_LINUX -lz
+clang++ -g main.cpp file.cpp mullib.cpp net.cpp serialize.cpp -lGL -lGLU -lSDL2 -lpthread -D_LINUX -lz
